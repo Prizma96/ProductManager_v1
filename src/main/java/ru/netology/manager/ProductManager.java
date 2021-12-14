20e0be13a7c2a9432914141f4333988366da7e43
Dasha <dashuta.tkachenko@list.ru>
2021-12-14 15:15:48 +0300

    correct commit

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -1,2 +1,57 @@
-package manager;public class ProductManager {
+package ru.netology.manager;
+
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+    private ProductRepository repo;
+
+    public ProductManager(ProductRepository repo) {
+        this.repo = repo;
+    }
+
+    public void add(Product product) {
+        repo.save(product);
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+
+        Product[] products = repo.findAll();
+
+        for (Product product : products) {
+            if (matches(product, text)) {
+                int lenght = result.length + 1;
+                Product[] tmp = new Product[lenght];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+                lenght++;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        }
+
+        if (product instanceof Book) { // если в параметре product лежит объект класса Book
+            Book book = (Book) product; // положим его в переменную типа Book чтобы пользоваться методами класса Book
+            if (book.getAuthor().contains(search)) { // проверим есть ли поисковое слово в данных об авторе
+                return true;
+            }
+        }
+        if (product instanceof Smartphone) {
+            Smartphone smartphone = (Smartphone) product;
+            if (smartphone.getProducter().contains(search)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
